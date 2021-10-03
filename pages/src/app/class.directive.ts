5de827b0e017a0af7dca46170fb91d57a8f272b6
Nestor Gonzalez <nestor.ge222@gmail.com>
2021-10-02 18:26:25 -0600

    Directivas

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -7,7 +7,17 @@ export class ClassDirective {
   //@Input() backgroundColor: string | undefined;
   constructor(private element: ElementRef) {}
 
-  @Input('appClass') set backgroundColor(value: string) {
+  /*   @Input('appClass') set backgroundColor(value: string) {
     this.element.nativeElement.style.backgroundColor = 'orage';
+  } */
+
+  @Input('appClass') set className(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.clasList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
